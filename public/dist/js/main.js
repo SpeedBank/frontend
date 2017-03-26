73d071bd403a7e240c89b42244b581af075715c9
andela-cdike <chukwuerika.dike@andela.com>
2017-03-26 06:44:04 +0100

    Add interactivity to the chat widget

--- public/dist/js/main.js
+++ public/dist/js/main.js
@@ -1,3 +1,7 @@
+
+const ACTIVECHAT = 'activeChat';
+const LASTQUERY = 'lastQuery';
+const CHAT_API_ERROR_MESSAGE = 'Something went wrong, Please try again!';
 (function ($) {
   $('a.page-scroll').bind('click', function (event) {
     var $anchor = $(this);
@@ -59,6 +63,110 @@
       $(this).stop().animate({ opacity: 0.5 }, 200);
     });
   });
-    
+
+  $('.panel-body').bind('DOMSubtreeModified', function() {
+    const s = $('.messages li').last()[0];
+    s.scrollIntoView();
+  });
+
+  $('#btn-input').on('keyup', function (e) {
+    if (e.keyCode === 13) {
+      handleChatSubmit();
+    }
+  });
+
+  $('#btn-chat').on('click', function (e) {
+    handleChatSubmit();
+  });
+
+  function injectNewResponsesIntoChat(payload) {
+    payload.forEach(message => {
+      $('ul.messages').append(prepareChatItem('right', message));
+    });
+  }
+
+  function resetActiveQueryInStorage() {
+    localStorage.removeItem(ACTIVECHAT);
+    localStorage.removeItem(LASTQUERY);
+  }
+
+  function postQueryToApi(query) {
+    $.post('https://speedbank.herokuapp.com/',
+      {
+        name: 'Donald Duck',
+        city: 'Duckburg'
+      },
+      (data, status) => {
+        if (status === 200) {
+          injectNewResponsesIntoChat(data);
+        } else {
+          injectNewResponsesIntoChat([CHAT_API_ERROR_MESSAGE]);
+        }
+      }
+    );
+    resetActiveQueryInStorage();
+  }
+
+  function getQueryToApi(query) {
+    $.get('https://speedbank.herokuapp.com/', function (result, status) {
+      if (status === 200) {
+        injectNewResponsesIntoChat(result);
+      } else {
+        injectNewResponsesIntoChat([CHAT_API_ERROR_MESSAGE]);
+      }
+    });
+  }
+
+  function injectUsersChat(query) {
+    const localMessage = prepareChatItem('left', query);
+    $('ul.messages').append(localMessage);
+    $('#btn-input').val('');
+  }
+
+  function handleChatSubmit() {
+    const query = document.getElementById('btn-input').value;
+    injectUsersChat(query);
+    if (query.toLowerCase() === 'yes' && localStorage.getItem(ACTIVECHAT)) {
+      return;
+      // return postQueryToApi(localStorage.getItem(LASTQUERY));
+    }
+    const remoteMessages = ['We create accounts', 'We take card', 'I can help'];
+    injectNewResponsesIntoChat(remoteMessages);
+
+    // save query to localStorage
+    localStorage.setItem(ACTIVECHAT, true);
+    localStorage.setItem(LASTQUERY, query);
+    setTimeout(() => {
+      const message = ['Not satisfied? Send <strong>YES</strong> for more information', '<strong>Send YES</strong>'];
+      injectNewResponsesIntoChat(message);
+    }, 6000);
+  }
 })(jQuery);
 
+function prepareChatItem(position, message) {
+  const url = {
+    left: '/dist/img/avatar3.png',
+    right: '',
+  };
+  const imgSrc = `<img src="${url[position]}">`;
+  const imgTag = url[position] ? imgSrc : '';
+  const element = `
+    <li class="message ${position} appeared">
+      <div class="avatar">${imgTag}</div>
+      <div class="text_wrapper">
+        <div class="text">${message}</div>
+      </div>
+    </li>
+  `;
+  return element;
+}
+
+function convertCookieToObject(str) {
+  str = str.split(', ');
+  const result = {};
+  for (let i = 0; i < str.length; i += 1) {
+    const cur = str[i].split('=');
+    result[cur[0]] = cur[1];
+  }
+  return result;
+}
